f86314674e8e399d6ef65ec2e71ebb8b0c1c7171
coolbele <64392071+coolbele@users.noreply.github.com>
2023-04-01 09:35:35 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -9,5 +9,6 @@
 sed -i 's/192.168.1.1/10.0.10.1/g' openwrt/package/base-files/files/bin/config_generate
 sed -i "/helloworld/d" "feeds.conf.default"
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome/
-git clone https://github.com/kenzok8/openwrt-packages.git
-git clone https://github.com/kenzok8/small.git
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+#git clone https://github.com/kenzok8/openwrt-packages.git
+#git clone https://github.com/kenzok8/small.git
