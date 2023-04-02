ca7aecf1845b87485cd3c82596a3aa1094270bd4
coolbele <64392071+coolbele@users.noreply.github.com>
2023-04-02 16:07:05 +0800

    Update diy-by-packages.sh

--- shell/diy-by-packages.sh
+++ shell/diy-by-packages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git" >> ./feeds.conf.default
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
 git pull
